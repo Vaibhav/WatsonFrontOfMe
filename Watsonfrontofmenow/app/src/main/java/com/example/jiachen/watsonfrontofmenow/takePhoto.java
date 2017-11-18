daa302be896d5aee386ec34df978a3f0c6e33e8e
Jiachen Wang <Jiachen775@gmail.com>
2017-11-18 08:29:24 -0500

    removed og

--- Watsonfrontofmenow/app/src/main/java/com/example/jiachen/watsonfrontofmenow/takePhoto.java
+++ Watsonfrontofmenow/app/src/main/java/com/example/jiachen/watsonfrontofmenow/takePhoto.java
@@ -2,6 +2,7 @@ package com.example.jiachen.watsonfrontofmenow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class takePhoto extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class takePhoto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
+    }
+
+    public void suckmyAss(View view){
+        System.out.println("SUCK MY ASS");
     }
 }
