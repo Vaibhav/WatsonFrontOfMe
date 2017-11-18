9e735b7f78c64b60ef87e31612c748ce1cba634e
Vaibhav <ishuvaibhav@gmail.com>
2017-11-18 06:01:12 -0500

    wtf

--- Watsonfrontofme/app/src/main/java/com/example/jiachen/watsonfrontofme/TakePhoto.java
+++ Watsonfrontofme/app/src/main/java/com/example/jiachen/watsonfrontofme/TakePhoto.java
@@ -1,8 +1,12 @@
 package com.example.jiachen.watsonfrontofme;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import static com.ibm.watson.developer_cloud.android.library.camera.CameraHelper.REQUEST_IMAGE_CAPTURE;
 
 public class TakePhoto extends AppCompatActivity {
 
--- Watsonfrontofme/app/src/main/java/com/example/jiachen/watsonfrontofme/identifyImage.java
+++ Watsonfrontofme/app/src/main/java/com/example/jiachen/watsonfrontofme/identifyImage.java
@@ -2,34 +2,42 @@ package com.example.jiachen.watsonfrontofme;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifierOptions;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualRecogniton;
+import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
 
 /**
  * Created by Vaibhav on 2017-11-18.
  */
 
-//ServiceCall<ClassifiedImages> classify(ClassifyOptions classifyOptions)
+
 
 
 public class identifyImage {
-    VisualRecognition service = new VisualRecognition(
-            VisualRecognition.VERSION_DATE_2016_05_20
-    );
+
+
+
+    public identifyImage() throws FileNotFoundException {
+
+        VisualRecognition service = new VisualRecognition(
+                VisualRecognition.VERSION_DATE_2016_05_20
+        );
         service.setApiKey("8d7aced8efa9ce11cca985d203dce5989cc20148");
 
-    InputStream imagesStream = new FileInputStream("./fruitbowl.jpg");
-    ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
-            .imagesFile(imagesStream)
-            .imagesFilename("fruitbowl.jpg")
-            .parameters("{\"classifier_ids\": [\"fruits_1462128776\","
-                    + "\"SatelliteModel_6242312846\"],"
-                    + "\"owners\": [\"IBM\", \"me\"]}")
-            .build();
-    ClassifiedImages result = service.classify(classifyOptions).execute();
+        InputStream imagesStream = new FileInputStream("./banana.jpg");
+        ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+                .imagesFile(imagesStream)
+                .imagesFilename("fruitbowl.jpg")
+                .parameters("{\"classifier_ids\": [\"fruits_1462128776\","
+                        + "\"SatelliteModel_6242312846\"],"
+                        + "\"owners\": [\"IBM\", \"me\"]}")
+                .build();
+        ClassifiedImages result = service.classify(classifyOptions).execute();
         System.out.println(result);
+    }
+
 }
